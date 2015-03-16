ede14411eabbe2099612a13000ea91f130cd8fb5
cerebis <matt.demaere@gmail.com>
2015-03-17 10:38:10 +1100

    switched from inheritance of dict to encapsulation

--- simulation/pipeline/bin/truthtable.py
+++ simulation/pipeline/bin/truthtable.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from collections import OrderedDict, Iterable
 import yaml
 
 
-class TruthTable(dict):
+class TruthTable:
     """
     Class which represents a truth table in the general sense.
     This can be either a prediction or the ground truth.
@@ -13,35 +14,57 @@ class TruthTable(dict):
     to a single most significant assignment. It is up to the user
     supply to choose a useful support value.
     """
-    def __init__(self, *args, **kwargs):
-        super(TruthTable, self).__init__(*args, **kwargs)
+    def __init__(self):
+        self.asgn_dict = {}
 
     def soft(self):
         """
         :return plain dictionary with degenerate classification
         """
-        _s = {}
-        _keys = sorted(self.keys())
+        _s = OrderedDict()
+        _keys = sorted(self.asgn_dict.keys())
         for k in _keys:
-            _s[k] = sorted(self[k].keys())
+            _s[k] = sorted(self.asgn_dict[k].keys())
         return _s
 
     def hard(self):
         """
         :return plain dictionary with the single most significant classification only.
         """
-        _s = {}
-        _keys = sorted(self.keys())
+        _s = OrderedDict()
+        _keys = sorted(self.asgn_dict.keys())
         for _k in _keys:
-            _v = self[_k]
+            _v = self.asgn_dict[_k]
             _v = sorted(_v, key=_v.get, reverse=True)
             _s[_k] = _v[0]
         return _s
 
+    def get(self, key):
+        return self.asgn_dict.get(key)
+
+    def put(self, key, value):
+        self.asgn_dict[key] = value
+
+    def update(self, dt):
+        for k, v in dt.iteritems():
+            self.asgn_dict[k] = v
+
+    def add(self, key, value):
+        self.asgn_dict[key] = value
+
+    def to_vector(self):
+        vec = {}
+        keys = sorted(self.asgn_dict.keys())
+        hd = self.hard()
+        for k in keys:
+            vec[k] = hd[k]
+        return vec
+
+
     @staticmethod
     def _write(table, pathname):
         with open(pathname, 'w') as h_out:
-            yaml.dump(table, h_out)
+            yaml.dump(table.asgn_dict, h_out, default_flow_style=False)
 
     def write(self, pathname):
         """
@@ -73,7 +96,9 @@ def read_truth(pathname):
     :return: truth table
     """
     with open(pathname, 'r') as h_in:
-        return yaml.load(h_in)
+        tt = TruthTable()
+        tt.update(yaml.load(h_in))
+        return tt
 
 
 def read_mcl(pathname):
@@ -87,39 +112,43 @@ def read_mcl(pathname):
         for n, line in enumerate(h_in, start=1):
             objs = line.rstrip().split()
             for o in objs:
-                mcl[o] = {n: None}
+                mcl.add(o, {n: None})
     return mcl
 
 
-def crosstab(tt1, tt2):
+def unique_labels(dt):
     """
-    Cross-tabulate two truth tables on hard clusters. Call tt.hard() first.
+    Extract the unique set of class labels used in the truth table
+    :param dt: dictionary representation of truth table to analyze
+    :return: sorted set of unique labels
+    """
+    labels = set()
+    for v in dt.values():
+        if isinstance(v, Iterable):
+            labels.update(v)
+        else:
+            labels.add(v)
+    return sorted(labels)
 
-    :param tt1: first truth table
-    :param tt2: second truth table
+
+def crosstab(dt1, dt2):
+    """
+    Cross-tabulate two truth tables on hard clusters.
+
+    :param dt1: first dictionary rep of truth table
+    :param dt2: second dictionary rep of truth table
     :return: pandas dataframe
     """
-    joined_keys = sorted(set(tt1.keys() + tt2.keys()))
-    rows = sorted(set(tt1.values()))
-    cols = sorted(set(tt2.values()))
+    joined_keys = sorted(set(dt1.keys() + dt2.keys()))
+
+    rows = unique_labels(dt1)
+    cols = unique_labels(dt2)
     ctab = pd.DataFrame(0, index=rows, columns=cols)
 
     for k in joined_keys:
-        if k in tt1 and k in tt2:
-            i1 = tt1[k]
-            i2 = tt2[k]
+        if k in dt1 and k in dt2:
+            i1 = dt1[k]
+            i2 = dt2[k]
             ctab.loc[i1, i2] += 1
 
     return ctab
-
-
-def ordereddict_rep(dumper, data):
-    """
-    Helper method to handling OrderedDict types in YAML dump.
-
-    :param dumper:
-    :param data:
-    :return:
-    """
-    return dumper.represent_mapping(u'tag:yaml.org,2002:map', data.items(), flow_style=False)
-
