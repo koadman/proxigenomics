2ff42cd4fb6039ab22700446e5b3f0cce352a6d1
cerebis <matt.demaere@gmail.com>
2015-03-17 10:40:47 +1100

    Simple utility for generating truth table statistics

---
+++ simulation/pipeline/bin/truth_stat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import truthtable as tt
+import numpy as np
+import sys
+
+table = tt.read_truth(sys.argv[1])
+num_class = []
+for k,v in table.iteritems():
+    num_class.append(len(v))
+
+print len(num_class), np.mean(num_class), np.std(num_class)
+
