fbbeaa1d25f13228dbca5e4761a86877d3658f48
Matthew DeMaere <matt.demaere@gmail.com>
2015-05-06 11:24:25 +1000

    changed handling of metis output with no cl assignments

--- simulation/pipeline/bin/metisClToMCL.py
+++ simulation/pipeline/bin/metisClToMCL.py
@@ -2,47 +2,74 @@
 import sys
 import argparse
 from collections import Counter
+import pprint as pp
 
-def metisCl_to_mcl(node_map, cluster_file, output, grabbag=False):
-    node_map = {}
+class IntegerSeries:
+    '''
+    Simple object to issue integer ids. I am sure this exists somewhere in Python
+    All series begin at 0
+    '''
+    def __init__(self, inc=True, step=1):
+        '''
+        Ascending or descending integers using a given step size
+        :param inc: True=ascending, False=descending
+        :param step: size of increment
+        '''
+        self.current = 0
+        self.inc = inc
+        self.step = step
+
+    @property
+    def next(self):
+        '''
+        Generate the next id and return it.
+        :return: next id in series
+        '''
+        if self.inc:
+            self.current += self.step
+        else:
+            self.current -= self.step
+        return self.current
+
+
+def metisCl_to_mcl(node_id_to_name, cluster_file, output):
+
+    node_id_to_name = {}
     for line in args.node_map[0]:
-        tok = line.rstrip().split()
-        if len(tok) != 2:
+        cl_assigned = line.rstrip().split()
+        if len(cl_assigned) != 2:
             sys.stderr.write('Error: invalid line did not contain 2 fields [{0}]\n'.format(line))
-        node_map[int(tok[0])] = tok[1]
+        node_id_to_name[int(cl_assigned[0])] = cl_assigned[1]
 
     # create a dictionary of scaffold => cluster
-    cl_set = Counter()
-    isolated_nodes = []
-    node_to_cl = {}
+    cl_table = Counter()
+    iso_id = IntegerSeries(inc=False)
+    node_name_to_cl = {}
     for n, line in enumerate(args.cluster_file[0], start=1):
-        tok = [int(ti) for ti in line.rstrip().split()]
-        if len(tok) == 0:
-            isolated_nodes.append(node_map[n])
-        else:
-            node_to_cl[node_map[n]] = tok
-            cl_set.update(tok)
+        cl_assigned = [int(ti) for ti in line.rstrip().split()]
+        if len(cl_assigned) == 0:
+            cl_assigned = [iso_id.next]
+            if iso_id.current in cl_table:
+                raise RuntimeError('Generated unique id:{0} for nid:{1} already existed'.format(n, iso_id.current))
+        node_name_to_cl[node_id_to_name[n]] = cl_assigned
+        cl_table.update(cl_assigned)
 
-    print 'Clustering breakdown: {0}'.format(cl_set)
+    print '\nClustering breakdown'
+    print 'BEGIN_TABLE'
+    print 'cluster\tcount'
+    for clid, count in cl_table.items():
+        print '{0}\t{1}'.format(clid, count)
+    print 'END_TABLE\n'
 
     # invert the dictionary, to cluster => scaffold
-    cl_to_node = dict.fromkeys(cl_set)
-    for k, v in node_to_cl.iteritems():
+    cl_to_node = dict.fromkeys(cl_table)
+    for k, v in node_name_to_cl.iteritems():
         for clid in v:
             cl = cl_to_node.get(clid)
             if cl is None:
                 cl = list()
                 cl_to_node[clid] = cl
             cl.append(k)
-
-    # insert nodes which were unassigned as each being in their own cluster or in a single grab-bag
-    nid = max(cl_set.keys()) + 1
-    if args.grabbag:
-        cl_to_node[nid] = isolated_nodes
-    else:
-        for node in isolated_nodes:
-            cl_to_node[nid] = [node]
-            nid += 1
 
     # sort clusters by descending size
     cl2scf = sorted(cl_to_node.values(), key=len, reverse=True)
@@ -54,8 +81,6 @@ def metisCl_to_mcl(node_map, cluster_file, output, grabbag=False):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Convert Metis-style clustering file to MCL format')
-    parser.add_argument('-g','--grabbag', action='store_true', default=False,
-                        help='Place unassigned nodes in a single grab-bag')
     parser.add_argument('node_map', type=argparse.FileType('r'), nargs=1,
                         help='node-map file, implicit ids to names')
     parser.add_argument('cluster_file', type=argparse.FileType('r'), nargs=1,
