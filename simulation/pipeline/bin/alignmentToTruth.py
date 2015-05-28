0e29bf8bac70278575a058e0a2ad73433f2ac229
Matthew DeMaere <matt.demaere@gmail.com>
2015-05-28 10:19:53 +1000

    extra runtime info

--- simulation/pipeline/bin/alignmentToTruth.py
+++ simulation/pipeline/bin/alignmentToTruth.py
@@ -221,6 +221,8 @@ def parse_psl(psl_file):
     :param psl_file: PSL format alignment file
     :return: dictionary of Alignment objects
     """
+    all_hits = 0
+    rejected = 0
     align_repo = OrderedDict()
     with open(psl_file, 'r') as h_in:
         for l in h_in:
@@ -228,13 +230,14 @@ def parse_psl(psl_file):
             if not psl_dataline.match(l):
                 continue
 
+            all_hits += 1
+
             fields = l.rsplit()
 
             qname = fields[9]
             rname = fields[13]
             alen = int(fields[12]) - int(fields[11]) + 1
             qlen = int(fields[10])
-            perid = (1.0 - float(fields[1]) / float(fields[0])) * 100
 
             # Taken from BLAT perl script for calculating percentage identity
             matches = int(fields[0])
@@ -246,6 +249,7 @@ def parse_psl(psl_file):
             # ignore alignment records which fall below mincov or minid
             # wrt the length of the alignment vs query sequence.
             if float(alen)/float(qlen) < args.mincov or perid < args.minid:
+                rejected += 1
                 continue
 
             aln = Alignment(qname, rname, alen, qlen, perid)
@@ -253,6 +257,10 @@ def parse_psl(psl_file):
                 align_repo[aln].add_bases(alen)
             else:
                 align_repo[aln] = aln
+
+        print 'Rejected {0}/{1} alignments due to constraints on ID {2} and Coverage {3}'.format(
+            rejected, all_hits, args.minid, args.mincov)
+
     return align_repo
 
 if __name__ == '__main__':
@@ -296,7 +304,7 @@ if __name__ == '__main__':
     if args.ofmt == 'flat':
         print 'Soft results always enabled for flat output format'
 
-    print 'Read {0} alignments'.format(len(align_repo))
+    print 'Accepted {0} alignments'.format(len(align_repo))
 
     #
     # We need to decide on assignment.
