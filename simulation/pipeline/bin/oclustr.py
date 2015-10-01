7f252dd39d2fdcd451bba99c1086e695796ec9d8
Matthew DeMaere <matt.demaere@gmail.com>
2015-10-01 13:58:54 +1000

    important fixes to oclustr implementation

--- simulation/pipeline/bin/oclustr.py
+++ simulation/pipeline/bin/oclustr.py
@@ -5,25 +5,22 @@ import argparse
 
 
 def approx_intra_sim(v, g):
-    v_star = g[v]
-    deg_vstar = len(v_star)
-    if deg_vstar < 1:
-        raise RuntimeError('Node degree is than one for node {0}'.format(v))
-    # denominator in paper is |v|-1 but doing so means we must exclude all
-    # degree 1 nodes (not just isolated |v|=0 nodes)
-    return np.sum([dat['weight'] for u1, u2, dat in g.edges(v, data=True) if u2 is not v]) / float(deg_vstar)
+    card_v = g.degree(v) + 1.
+    if card_v <= 1:
+        raise RuntimeError('Cardinality of V* is less than two for node {0}'.format(v))
+    return np.sum([dat['weight'] for u1, u2, dat in g.edges(v, data=True) if u2 is not v]) / (card_v - 1.)
 
 
 def relative_density(v, g):
     v_star = g[v]
-    deg_v = len(v_star)
-    density_v = len([u for u in v_star if len(g[u]) < deg_v])
+    deg_v = g.degree(v)
+    density_v = len([u for u in v_star if g.degree(u) <= deg_v])
     return density_v / float(deg_v)
 
 
 def relative_compactness(v, g):
     v_star = g[v]
-    deg_v = len(v_star)
+    deg_v = g.degree(v)
     sim_v = approx_intra_sim(v, g)
     compact_v = len([u for u in v_star if u is not v and sim_v >= approx_intra_sim(u, g)])
     return compact_v / float(deg_v)
@@ -36,7 +33,7 @@ def relevance(v, g):
 class Cover:
     def __init__(self, g):
         self.g = g
-        self.hubs = nx.Graph()
+        self.hubs = nx.DiGraph()
 
     def update(self, vlist):
         for v in vlist:
@@ -55,7 +52,7 @@ class Cover:
         self.hubs.node[v]['owned_by'].add(v)
         self.hubs.node[v]['hub'] = True
 
-        v_adj = g[v].keys()
+        v_adj = self.g[v].keys()
         for vi in v_adj:
             # for each satellite, create new node or update existing.
             # we might be covering the same node with more than one star
@@ -71,9 +68,9 @@ class Cover:
         :param v: the hub vertex
         :return: the list of shared satellites
         """
-        v_adj = self.g[v]
+        v_adj = self.hubs[v]
         # vertices with more than one owner
-        return [u for u in v_adj if len(self.hubs.node[u]['owned_by']) > 1]
+        return [ui for ui in v_adj if self.hubs.degree(ui) > 1]
 
     def unshared(self, v):
         """
@@ -82,9 +79,9 @@ class Cover:
         :param v: the hub vertex
         :return: the list of unshared satellites
         """
-        v_adj = self.g[v]
+        v_adj = self.hubs[v]
         # vertices with only 1 owner
-        return [u for u in v_adj if len(self.hubs.node[u]['owned_by']) <= 1]
+        return [ui for ui in v_adj if self.hubs.degree(ui) <= 1]
 
     def is_useful(self, v):
         """
@@ -97,74 +94,77 @@ class Cover:
         nu = len(self.unshared(v))
         return ns <= nu
 
-    def highest_containing(self, u):
-        """
-        For a given vertex u, find which owning hub has the highest degree. If there is
-        tie between owning hubs, pick one at random.
-        :param u: the vertex to test.
-        :return: the hub vertex with high degree
-        """
-        owners = self.hubs.node[u]['owned_by']
+    # def highest_containing(self, u):
+    #     """
+    #     For a given vertex u, find which owning hub has the highest degree. If there is a
+    #     tie between owning hubs, pick one at random.
+    #     :param u: the vertex to test.
+    #     :return: the hub vertex with high degree
+    #     """
+    #     owners = self.hubs.node[u]['owned_by']
+    #
+    #     # owners degrees, not including self
+    #     owner_deg = dict((v, self.g.degree(v)) for v in owners if v != u)
+    #
+    #     if len(owner_deg) == 0:
+    #         raise RuntimeWarning('{0} is an isolated vertex containing only itself'.format(u))
+    #     elif len(owner_deg) == 1:
+    #         # only one non-self owner
+    #         return owner_deg.popitem()
+    #     else:
+    #         # sort owners by descending degree
+    #         deg_desc = sorted(owner_deg, key=owner_deg.get, reverse=True)
+    #         top = [owner_deg[deg_desc[0]], owner_deg[deg_desc[1]]]
+    #         if top[0] == top[1]:
+    #             # in event of tie, pick winner at random
+    #             idx = np.random.choice(deg_desc[:2])
+    #             return idx, owner_deg[idx]
+    #         else:
+    #             return deg_desc[0], top[0]
 
-        # owners degrees, not include self
-        owner_deg = dict((v, self.g.degree(v)) for v in owners if v != u)
+    # def demote(self, target):
+    #     """
+    #     Demote a node from being a hub. This means all subordinate nodes
+    #     which recognised this node as a hub, are also updated.
+    #     :param target: the target hub to remove
+    #     """
+    #     # for adjacent satellites vi of hub v
+    #     adj_list = set(self.hubs[target])
+    #     for adj in adj_list:
+    #         adj_dat = self.hubs.node[adj]
+    #         if target in adj_dat['owned_by']:
+    #             # Remove target from list of owners of vadj
+    #             adj_dat['owned_by'].remove(target)
+    #
+    #         # ?? this will cut off hub node from all its adjacent nodes
+    #         self.hubs.remove_edge(target, adj)
+    #     self.hubs.node[target]['owned_by'].remove(target)
+    #     self.hubs.node[target]['hub'] = False
+    #
+    #     if self.hubs.degree(target) == 0:
+    #         print '{0} was demoted and now has zero degree. Data: {1}'.format(target, self.hubs.node[target])
 
-        if len(owner_deg) == 0:
-            raise RuntimeWarning('{0} is an isolated vertex containing only itself'.format(u))
-        elif len(owner_deg) == 1:
-            # only one non-self owner
-            return owner_deg.popitem()
-        else:
-            # sort owners by descending degree
-            deg_desc = sorted(owner_deg, key=owner_deg.get, reverse=True)
-            top = [owner_deg[deg_desc[0]], owner_deg[deg_desc[1]]]
-            if top[0] == top[1]:
-                # in event of tie, pick winner at random
-                idx = np.random.choice(deg_desc[:2])
-                return idx, owner_deg[idx]
-            else:
-                return deg_desc[0], top[0]
-
-    def demote(self, v):
-        """
-        Demote a node from being a hub. This means all subordinate nodes
-        which recognised this node as a hub, are also updated.
-        :param v: the target hub to remove
-        """
-        # for adjacent satellites vi of hub v
-        v_adj = set(self.hubs[v])
-        for vi in v_adj:
-            vi_dat = self.hubs.node[vi]
-            if v in vi_dat['owned_by']:
-                # TODO this may not be necessary, the logic should result in statement always being true
-                vi_dat['owned_by'].remove(v)
-            self.hubs.remove_edge(v, vi)
-
-    def migrate(self, vlist, from_v, to_v):
-        """
-        Update the edges and attributes of a list of nodes to
-        reflect the change of ownership.
-        :param vlist: the list of target vertices to move
-        :param from_v: the origin hub vertex
-        :param to_v: the destination hub vertex
-        :return:
-        """
-        for vi in vlist:
-            # avoid moving self
-            if vi != from_v:
-                self.hubs.remove_edge(from_v, vi)
-                self.hubs.add_edge(to_v, vi)
-                self.hubs.node[vi]['owned_by'].remove(from_v)
-                self.hubs.node[vi]['owned_by'].add(to_v)
-
-    def is_hub(self, v):
-        return self.hubs.node[v]['hub']
-
-    def is_analyzed(self, v):
-        return self.hubs.node[v]['analyzed']
-
-    def mark_analyzed(self, v):
-        self.hubs.node[v]['analyzed'] = True
+    # def migrate(self, vlist, from_v, to_v):
+    #     """
+    #     Update the edges and attributes of a list of nodes to
+    #     reflect the change of ownership.
+    #     :param vlist: the list of target vertices to move
+    #     :param from_v: the origin hub vertex
+    #     :param to_v: the destination hub vertex
+    #     :return:
+    #     """
+    #     for vi in vlist:
+    #         # avoid moving self
+    #         print 'Moving {0} from {1} to {2}'.format(vi, from_v, to_v)
+    #         if vi != from_v:
+    #             try:
+    #                 self.hubs.remove_edge(from_v, vi)
+    #                 self.hubs.add_edge(to_v, vi)
+    #                 self.hubs.node[vi]['owned_by'].remove(from_v)
+    #                 self.hubs.node[vi]['owned_by'].add(to_v)
+    #             except nx.NetworkXError as er:
+    #                 print self.hubs[from_v]
+    #                 raise er
 
     def exists(self, v):
         """
@@ -190,19 +190,48 @@ class Cover:
         :param no_isolates:
         :return:
         """
+        # make a copy to modify
         gout = self.hubs.copy()
 
+        # prune isolates if requested
         if no_isolates:
-            iso_v = [v for v in gout if gout.degree(v) == 0]
+            iso_v = [v for v, dat in gout.nodes(data=True) if 'isolate' in dat and dat['isolate']]
             gout.remove_nodes_from(iso_v)
 
         for v in gout:
-            # TODO
-            # networkx cannot write collections as attributes
-            # so we're converting to a string for now.
-            gout.node[v]['owned_by'] = ' '.join(gout.node[v]['owned_by'])
+            # TODO networkx cannot write collections as attributes, so we're converting to a string for now.
+            gout.node[v]['owned_by'] = ' '.join(gout.predecessors(v))
+            gout.node[v].update(self.g.node[v])
 
         nx.write_graphml(gout, path)
+
+    def write_mcl(self, path, no_isolates=False):
+        # write MCL format cluster solution
+        with open(path, 'w') as out:
+            min_deg = 1 if no_isolates else 0
+            # build a list of hubs and their degree, sort be descending degree for output
+            hub_dict = dict((v, self.hubs.degree(v)) for v in self.hubs
+                            if self.hubs.node[v]['hub'] and self.hubs.degree(v) >= min_deg)
+            desc_deg = sorted(hub_dict, key=hub_dict.get, reverse=True)
+            for vi in desc_deg:
+                out.write('{0} {1}\n'.format(vi, ' '.join([str(vid) for vid in self.hubs[vi]])))
+            print 'Final solution: {0} stars and {1} satellites'.format(len(hub_dict), sum(hub_dict.values()))
+
+    def add_novel_adjacent(self, center, adj_list):
+        """
+        For a given center node, add all novel adjacent nodes supplied. Edges will
+        be directed from the center hub to the node. Novelty is defined as nodes
+        which currently do not exist in the cover graph.
+
+        :param center: the vertex at the center (hub)
+        :param adj_list: the list of adjacent nodes.
+        """
+        for vi in adj_list:
+            if vi not in self.hubs:
+                # add node with attributes
+                self.hubs.add_node(vi, hub=False, analyzed=False)
+            # directed edge from hub to node
+            self.hubs.add_edge(center, vi, linked=False)
 
     def __str__(self):
         return str(self.hubs)
@@ -210,6 +239,111 @@ class Cover:
     def __repr__(self):
         return repr(self.hubs)
 
+def relevance_by_node(g, desc=True):
+    # TODO make this an ordered dict and dispense with one container
+    rel_dict = dict((v, relevance(v, g)) for v in g.nodes() if len(g[v]) >= 1)
+    desc_rel = sorted(dict((k, v) for k, v in rel_dict.iteritems() if v > 0.0), key=rel_dict.get, reverse=desc)
+    return desc_rel, rel_dict
+
+def ocluster(g, debug=False):
+    print 'Graph contains {0} vertices, {1} edges'.format(g.order(), g.size())
+
+    # prune self-loops before starting analysis
+    for e in g.selfloop_edges():
+        g.remove_edge(*e)
+    print 'After removing self-loops, {0} edges remain'.format(g.size())
+
+    desc_relevance, rel_dict = relevance_by_node(g, True)
+
+    if debug:
+        print 'Vertices sorted by decreasing relevance'
+        for n, li in enumerate(desc_relevance, start=1):
+            print '{0} {1} {2:.3f}'.format(n, li, rel_dict[li])
+
+    C = Cover(g)
+
+    # add isolated nodes first
+    C.hubs.add_nodes_from(set(v for v in g.nodes_iter() if g.degree(v) == 0), hub=False, isolate=True)
+    print 'There were {0} isolated vertices'.format(len(C.hubs))
+
+    # traverse the set of vertices in descending order of relevance
+    # looking for stars which cover new territory
+    n_hub = 0
+    for vi in desc_relevance:
+        if vi not in C.hubs:
+            # adding a new hub and adjacents
+            print '{0} is a new hub'.format(vi)
+            C.hubs.add_node(vi, hub=True, analyzed=False)
+            C.add_novel_adjacent(vi, g[vi])
+            n_hub += 1
+        else:
+            # check if adding this hub would cover new adjacent nodes
+            # build set of adjacent nodes and compare against all
+            vi_adj = set(g[vi])
+            cover_set = set(C.hubs.nodes())
+            if not vi_adj < cover_set:
+                # set contained some extra (IE not a subset of cover_set)
+                print '{0} covered another {1} adjacent'.format(vi, len(vi_adj - cover_set))
+                # status is now a hub, add adjacents to it.
+                C.hubs.node[vi]['hub'] = True
+                C.add_novel_adjacent(vi, vi_adj)
+                n_hub += 1
+
+    print 'Finished step 1: {0} hubs and {1} vertices'.format(n_hub, len(C.hubs))
+
+    # sort the ws-stars by descending degree
+    deg_dict = dict((vi, g.degree(vi)) for vi in C.hubs if C.hubs.node[vi]['hub'])  # hubs only
+    desc_degree = sorted(deg_dict, key=deg_dict.get, reverse=True)
+
+    if debug:
+        for n, v in enumerate(desc_degree, start=1):
+            if v in rel_dict:
+                print '{0} {1} {2:.3f} {3}'.format(n, v, rel_dict[v], deg_dict[v])
+            else:
+                print '{0} {1} NA {2}'.format(n, v, deg_dict[v])
+
+    linked = {}
+    demoted = []
+    print 'Iterating through hubs by descending degree, eliminate useless ws_stars'
+    # from largest hub to smallest, demote any hub which contains more shared
+    # than unshared nodes (IE not useful).
+    for vi in desc_degree:
+        if deg_dict[vi] <= 0:
+            continue
+
+        print '\tFor hub {0} with degree {1}'.format(vi, deg_dict[vi])
+
+        linked[vi] = set()
+        v_adj = set(C.hubs[vi])
+        for ui in v_adj:
+            if C.hubs.node[ui]['hub'] and not C.hubs.node[ui]['analyzed']:
+                if not C.is_useful(ui):
+                    print '\t\tOverlapping hub {0} was not useful as a ws_star'.format(ui)
+                    demoted.append(ui)
+                    unshared = C.unshared(ui)
+                    linked[vi].update(unshared)
+                    print '\t\tThere were {0} unshared nodes to relink'.format(len(unshared))
+                    u_adj = set(C.hubs[ui])
+                    for wi in u_adj:
+                        C.hubs.remove_edge(ui, wi)
+                    C.hubs.node[ui]['hub'] = False
+                else:
+                    C.hubs.node[ui]['analyzed'] = True
+
+    # Add re-link nodes that have become unconnected, attach them
+    # to the grandparent hub of their demoted parent hub.
+    for li in linked:
+        for vi in linked[li]:
+            if not C.hubs.node[li]['hub']:
+                raise RuntimeError('nodes linked to non-hub {0}'.format(li))
+            C.hubs.add_edge(li, vi, linked=True)
+
+    print 'Demoted {0} hub vertices, relinked {1} satellites'.format(len(demoted), len(linked))
+    if debug:
+        print 'Demoted hubs were: {0}'.format(demoted)
+        print 'Relinked vertices were: {0}'.format(linked)
+
+    return C
 
 if __name__ == '__main__':
 
@@ -223,85 +357,10 @@ if __name__ == '__main__':
 
     # read the graph
     g = nx.read_graphml(args.graph[0])
-    print 'Graph contains {0} vertices, {1} edges'.format(g.order(), g.size())
 
-    # prune self-loops before starting analysis
-    for e in g.selfloop_edges():
-        g.remove_edge(*e)
-    print 'After removing self-loops, {0} edges remain'.format(g.size())
-
-    # calculate relevance for all vertices
-    rel_dict = dict((v, relevance(v, g)) for v in g.nodes() if len(g[v]) >= 1)
-
-    # sort candidates by decreasing relevance
-    desc_relevance = sorted(dict((k, v) for k, v in rel_dict.iteritems() if v > 0.0), key=rel_dict.get, reverse=True)
-
-    if args.debug:
-        print 'Vertices sorted by decreasing relevance'
-        for n, li in enumerate(desc_relevance, start=1):
-            print '{0} {1} {2:.3f}'.format(n, li, rel_dict[li])
-
-    # add all isolated nodes
-    cover = Cover(g)
-    cover.update(set(v for v in g.nodes_iter() if g.degree(v) == 0))
-    print 'There were {0} isolated vertices'.format(len(cover.hubs))
-
-    # traverse the set of vertices in descending order of relevance
-    # looking for stars which cover new territory
-    n_hub = 0
-    for v in desc_relevance:
-        if not cover.exists(v):
-            # new hub of a star
-            cover.add(v)
-            n_hub += 1
-        elif not cover.contains(set(g[v].keys())):
-            # star had at least one new covered vertex
-            cover.add(v)
-            n_hub += 1
-
-    print 'Finished step 1: {0} stars and {1} vertices'.format(n_hub, len(cover.hubs))
-
-    # determine degree of each star and sort by descending degree
-    deg_dict = dict((v, g.degree(v)) for v in cover.hubs)
-    desc_degree = sorted(deg_dict, key=deg_dict.get, reverse=True)
-
-    if args.debug:
-        for n, v in enumerate(desc_degree, start=1):
-            if v in rel_dict:
-                print '{0} {1} {2:.3f} {3}'.format(n, v, rel_dict[v], deg_dict[v])
-            else:
-                print '{0} {1} NA {2}'.format(n, v, deg_dict[v])
-
-    # traverse the set of hubs by descending degree, if not useful, then
-    # rehome unshared satellites with highest degree overlapping hub.
-    demoted = []
-    migrated = []
-    # for each hub by descending degree
-    for v in desc_degree:
-        # get the members
-        v_adj = g[v]
-        for u in v_adj:
-            if cover.is_hub(u) and not cover.is_analyzed(u):
-                if not cover.is_useful(u):
-                    try:
-                        v_high, v_deg = cover.highest_containing(u)
-                        unshared = cover.unshared(u)
-                        if len(unshared) > 0:
-                            if args.debug:
-                                print 'Moving unshared vertices {0} from {1} to {2}'.format(unshared, u, v_high)
-                            cover.migrate(unshared, u, v_high)
-                            migrated.extend(unshared)
-                        cover.demote(u)
-                        demoted.append(u)
-                    except RuntimeWarning as wrn:
-                        if args.debug:
-                            print 'No action taken, {0}'.format(wrn.message)
-                cover.mark_analyzed(u)
-
-    print 'Demoted {0} hub vertices, migrated {1} satellites'.format(len(demoted), len(migrated))
-    if args.debug:
-        print 'Demoted hubs were: {0}'.format(demoted)
-        print 'Migrated vertices were: {0}'.format(migrated)
+    # create cover of g
+    cover = ocluster(g, args.debug)
+    print cover
 
     if args.format == 'graphml':
         # write cover graph to file
