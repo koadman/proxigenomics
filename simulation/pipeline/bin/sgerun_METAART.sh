4c027b673a8afea5c915045f9f1d3e5e34ae7ec0
Aaron Darling <aaron.darling@uts.edu.au>
2015-10-19 12:27:00 +1100

    initial support for many samples per community, each with different relative abundances

--- simulation/pipeline/bin/sgerun_METAART.sh
+++ simulation/pipeline/bin/sgerun_METAART.sh
@@ -35,7 +35,7 @@ if [ -z "$JOB_ID" ] # SUBMIT MODE
 then
 	if [ $# -ne 9 ]
 	then
-		echo "Usage: [seed] [insert len] [insert sd] [read len] [x-fold] [comm table] [ref sequences] [base_name] [out_dir]"
+		echo "Usage: [seed] [insert len] [insert sd] [read len] [x-fold] [num samples] [ref sequences] [base_name] [out_dir] [lognorm rel abundance mu] [lognorm rel abundance sigma]"
 		exit 1
 	fi
 	echo "Submitting run"
@@ -51,13 +51,13 @@ then
 	trap 'rollback_rm_files ${TARGET[@]}; exit $?' INT TERM EXIT
 
 	CMD=`readlink -f $0`
-	qsub -sync yes -V -b n -v SEED=$1,INSERT_LEN=$2,INSERT_SD=$3,READ_LEN=$4,X_FOLD=$5,COMM_TABLE=$6,REF_SEQ=$7,BASE_NAME=$8,OUT_DIR=$9 $CMD
+	qsub -sync yes -V -b n -v SEED=$1,INSERT_LEN=$2,INSERT_SD=$3,READ_LEN=$4,X_FOLD=$5,NUM_SAMPLES=$6,REF_SEQ=$7,BASE_NAME=$8,OUT_DIR=$9,LOGNORM_REL_ABUNDANCE_MU=$10,LOGNORM_REL_ABUNDANCE_SIGMA=$11 $CMD
 
 	trap - INT TERM EXIT
 	echo "Finished"
 
 else # EXECUTION MODE
-	echo "Running $MA_PATH --log logs/$PBS_JOBID.ma.log --art-path $ART_PATH -S $SEED -m $INSERT_LEN -s $INSERT_SD -l $READ_LEN -M $X_FOLD -t $COMM_TABLE $REF_SEQ     $OUT_DIR/wgs"
+	echo "Running $MA_PATH --log logs/$PBS_JOBID.ma.log --art-path $ART_PATH -S $SEED -m $INSERT_LEN -s $INSERT_SD -l $READ_LEN -M $X_FOLD -U $LOGNORM_REL_ABUNDANCE_MU -u $LOGNORM_REL_ABUNDANCE_SIGMA -n $NUM_SAMPLES $REF_SEQ     $OUT_DIR/wgs"
 
     $MA_PATH --log logs/$PBS_JOBID.ma.log --art-path $ART_PATH -S $SEED -m $INSERT_LEN -s $INSERT_SD -l $READ_LEN -M $X_FOLD -t $COMM_TABLE $REF_SEQ $OUT_DIR/wgs
 fi
